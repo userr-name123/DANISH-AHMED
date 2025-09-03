57731dc11a7cb33252f3140b11ac257ac300edce
userr-name123 <mdanish.ahmed3054@gmail.com>
2025-09-03 11:29:42 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,28 @@
+class Lab1P1{
+	public static void main(String...args){
+		short s = 15;
+		Short ss = s; //AutoBoxing
+		
+		
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(ss.TYPE);
+		System.out.println(ss.SIZE);
+		System.out.println(ss.BYTES);
+		
+		int i = 15;
+		Interger ii = 15 //AutoBoxing
+		
+		System.out.println(Interger.MAX_VALUE);
+		System.out.println(ii.MIN_VALUE);
+		System.out.println(ii.TYPE);
+		System.out.println(ii.SIZE);
+		Sytem.out.println(ii.BYTES);
+		
+		long l =15;
+		Long ll = l; //AutoBoxing
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(ll.MIN_VALUE);
+}
+} 
