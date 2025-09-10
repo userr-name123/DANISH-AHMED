ee36a651b230a5423b6ca2e897344263300d2678
userr-name123 <mdanish.ahmed3054@gmail.com>
2025-09-10 12:34:01 +0530

    Add files via upload

---
+++ Lab2P4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Lab2P4{
+	public static void main(String...args){
+	Scanner s = new Scanner(System.in);
+	
+		int a = s.nextInt();
+		int count = 0;
+		
+		int i = 2;
+		while(i<0){
+		
+		};
+		
+		System.out.println("Result is: "+ str);
+	}
+}	
