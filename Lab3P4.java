3ef8d4d5baf8b73784e5d87959a5c6ee8b16342f
userr-name123 <mdanish.ahmed3054@gmail.com>
2025-09-17 12:34:27 +0530

    Add files via upload

---
+++ Lab3P4.java
@@ -0,0 +1,18 @@
+class Lab3P4{
+	public static void main(String...args){
+	
+	}
+	Lab3P4(){
+		System.out.println("constructor without params");
+	}
+	Lab3P4(int x){
+		System.out.println("constructor with param"+x);
+	}
+	Lab3P4(String s){
+		System.out.println("constructor with string params"+s);
+	}
+	Lab3P4(int x, String s){
+		System.out.println("constructor with int and String params"+x,s);	
+	}
+	
+}
